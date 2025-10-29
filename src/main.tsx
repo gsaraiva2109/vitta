3759f118d4286b79db86b0cf36ed71d951715633
gsaraiva2109 <gsaraiva2109@gmail.com>
2025-10-29 17:50:19 -0300

    feat: implement machine management features including create, edit, view, and delete functionalities

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,9 @@ import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
 import './index.css'
 import AppRouter from './router'
 
-import 'primereact/resources/themes/lara-light-blue/theme.css'
+import 'primereact/resources/themes/lara-light-blue/theme.css'; // Tema claro com azul
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
