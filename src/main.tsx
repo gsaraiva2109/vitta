e08d5d6dee67dc41877062a2184fd56285be271e
ChangedRuby <gugasboy7@gmail.com>
2025-10-29 18:20:09 -0300

    fixed unused dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
-import { StrictMode, useContext } from 'react'
-import { createRoot } from 'react-dom/client'
-import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
-import './index.css'
-import AppRouter from './router'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { PrimeReactProvider } from "primereact/api";
+import "./index.css";
+import AppRouter from "./router";
 
-import 'primereact/resources/themes/lara-light-blue/theme.css'; // Tema claro com azul
-import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css';
+import "primereact/resources/themes/lara-light-blue/theme.css"; // Tema claro com azul
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <PrimeReactProvider>
       <AppRouter />
     </PrimeReactProvider>
   </StrictMode>,
-)
+);
