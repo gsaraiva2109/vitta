f80cd59153bdc95499d05bde3bf757fa14a17839
gsaraiva2109 <gsaraiva2109@gmail.com>
2025-11-09 14:28:12 -0300

    test: Trigger auto-deploy Coolify

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,7 +7,7 @@ import AppRouter from "./router";
 import "primereact/resources/themes/lara-light-blue/theme.css"; // Tema claro com azul
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
-
+// This is a test comment for auto-deploy
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <PrimeReactProvider>
