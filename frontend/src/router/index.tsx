4faf84d9d07201eb4dc913397a342120799d63b3
gsaraiva2109 <gsaraiva2109@gmail.com>
2025-11-16 17:37:50 -0300

    refactor: remove authentication context and protected route components

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -5,25 +5,19 @@ import Maquinas from '../views/Maquinas';
 import Manutencoes from '../views/Manutencoes';
 import Alertas from '../views/Alertas';
 import Relatorios from '../views/Relatorios';
-import ProtectedRoute from '../components/ProtectedRoute'; // Import ProtectedRoute
 
 const AppRouter = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/login" element={<Login />} />
-      <Route element={<ProtectedRoute />}> {/* Protected routes wrapper */}
-        <Route path="/" element={<Home />} />
-        <Route path="/maquinas" element={<Maquinas />} />
-        <Route path="/manutencoes" element={<Manutencoes />} />
-        <Route path="/alertas" element={<Alertas />} />
-        <Route path="/relatorios" element={<Relatorios />} />
-      </Route>
-      {/* Optionally, add a route for 404 Not Found */}
-      {/* <Route path="*" element={<NotFound />} /> */}
+      <Route path="/" element={<Home />} />
+      <Route path="/maquinas" element={<Maquinas />} />
+      <Route path="/manutencoes" element={<Manutencoes />} />
+      <Route path="/alertas" element={<Alertas />} />
+      <Route path="/relatorios" element={<Relatorios />} />
     </Routes>
   </BrowserRouter>
 );
 
-export default AppRouter;
 
 export default AppRouter;
