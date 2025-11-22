bbfd39f45d33bf45b4c526f24c1b6c8ccd296038
gsaraiva2109 <gsaraiva2109@gmail.com>
2025-11-22 20:36:57 -0300

    test(api): setup jest and supertest with initial tests

--- api/server.js
+++ api/server.js
@@ -75,6 +75,10 @@ connectDB().then(async () => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  logger.info(`Servidor rodando na porta ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'test') {
+  app.listen(PORT, () => {
+    logger.info(`Servidor rodando na porta ${PORT}`);
+  });
+}
+
+export default app;
