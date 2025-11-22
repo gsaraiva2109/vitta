cc2c337c12c0b835074570ab2735dca38a4943a1
gsaraiva2109 <gsaraiva2109@gmail.com>
2025-11-22 16:08:00 -0300

    fix: update CSS structure and imports; remove unused Tailwind config

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { PrimeReactProvider } from "primereact/api";
-import "./index.css";
-import AppRouter from "./router";
 
 import "primereact/resources/themes/lara-light-blue/theme.css"; // Tema claro com azul
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
+import "./index.css"; // Tailwind v4 deve vir DEPOIS do PrimeReact
+import "./custom-theme.css";
+
+import AppRouter from "./router";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <PrimeReactProvider>
