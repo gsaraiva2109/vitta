b5a6600884c34226d78668c9d3f0c7f382eadb29
gsaraiva2109 <gsaraiva2109@gmail.com>
2025-11-22 20:33:06 -0300

    refactor(api): implement centralized error handling

---
+++ api/middleware/errorMiddleware.js
@@ -0,0 +1,14 @@
+import logger from '../config/logger.js';
+
+export default function errorMiddleware(err, req, res, next) {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Erro interno do servidor';
+
+  logger.error(`${statusCode} - ${message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+
+  res.status(statusCode).json({
+    status: 'error',
+    statusCode,
+    message,
+  });
+}
