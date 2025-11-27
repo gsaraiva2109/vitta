14380748ed7e1fd9735c69384ff19bd2f5b1b230
ChangedRuby <gugasboy7@gmail.com>
2025-11-27 07:38:00 -0300

    chore: fix swagger

--- api/routes/alertaRoutes.js
+++ api/routes/alertaRoutes.js
@@ -4,6 +4,42 @@ import { getAlerts } from '../controllers/alertaController.js';
 
 const router = Router();
 
+/**
+ * @swagger
+ * tags:
+ *   name: Alertas
+ *   description: API para gerenciamento de alertas
+ */
+
+/**
+ * @swagger
+ * /api/alertas:
+ *   get:
+ *     summary: Retorna uma lista de todos os alertas
+ *     tags: [Alertas]
+ *     responses:
+ *       200:
+ *         description: Lista de alertas retornada com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                     description: ID do alerta
+ *                   message:
+ *                     type: string
+ *                     description: Mensagem do alerta
+ *                   timestamp:
+ *                     type: string
+ *                     format: date-time
+ *                     description: Data e hora do alerta
+ *       500:
+ *         description: Erro no servidor
+ */
 router.get('/', getAlerts);
 
 export default router;
